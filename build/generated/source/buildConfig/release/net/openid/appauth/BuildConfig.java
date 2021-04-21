05bb88671439651cf969ec6be0c905ed2a5dc227
Sebastien  Fourtou <sfourtou@dev-id.fr>
2021-04-21 14:56:31 +0200

    Initial commit

---
+++ build/generated/source/buildConfig/release/net/openid/appauth/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package net.openid.appauth;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "net.openid.appauth";
+  public static final String BUILD_TYPE = "release";
+}
